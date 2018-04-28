71fa82b5dfa6ff1fbc17517a7fe0e951ba6fd371
broomstar <broomstar@163.com>
2018-04-29 03:02:59 +0800

    fix

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -2,12 +2,11 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use bytes::BytesMut;
 use std::sync::{Arc, Mutex};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 #[allow(dead_code)]
-pub fn decrypt<T>(buffer: &mut T, iv: Arc<Mutex<BytesMut>>, size: usize)
+pub fn decrypt<T>(buffer: &mut T, iv: Arc<Mutex<Vec<u8>>>, size: usize)
 where
     T: AsMut<[u8]>,
 {
@@ -20,17 +19,17 @@ where
     }
 }
 #[allow(dead_code)]
-pub fn encrypt(buffer: &mut [u8], iv: &mut [u8], size: usize) {
+pub fn encrypt(buffer: &mut [u8], iv: Arc<Mutex<Vec<u8>>>, size: usize) {
     unsafe {
         ffi_encrypt(
-            buffer.as_mut_ptr(),
-            iv.as_mut_ptr(),
+            buffer.as_mut().as_mut_ptr(),
+            iv.lock().unwrap().as_mut_ptr(),
             size as ::std::os::raw::c_ushort,
         );
     }
 }
 #[allow(dead_code)]
-pub fn create_packet_header(buffer: &mut [u8], iv: Arc<Mutex<BytesMut>>, size: usize) {
+pub fn create_packet_header(buffer: &mut [u8], iv: Arc<Mutex<Vec<u8>>>, size: usize) {
     unsafe {
         ffi_create_packet_header(
             buffer.as_mut_ptr(),
